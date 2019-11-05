6966df4e57e6aff4b731506b423caf8b9dc0cfbb
ef69036 <emiliano.fagundes@southsystem.com.br>
2019-11-05 17:08:44 -0200

    Implementação inicial de testes

---
+++ src/main/java/br/com/test/mercadobitcoinapitesting/MercadoBitcoinApiTestingApplication.java
@@ -0,0 +1,13 @@
+package br.com.test.mercadobitcoinapitesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoBitcoinApiTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoBitcoinApiTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/test/mercadobitcoinapitesting/MercadoBiticoinApiTesting.java
@@ -0,0 +1,72 @@
+package br.com.test.mercadobitcoinapitesting;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.restassured.http.ContentType;
+/**
+ * Classe para testes de enpoints da API publica
+ * do Mercado Bitcoin.
+ * 
+ * A documentação referência utilizada para construção destes testes
+ * está disponível em:  https://www.mercadobitcoin.com.br/api-doc/
+ * @author Emiliano Fagundes
+ *
+ */
+@SpringBootTest
+public class MercadoBiticoinApiTesting {
+
+	
+	private final String BASE_URL = "https://www.mercadobitcoin.net/api/";
+	
+	/**
+	 * Teste que verifica se o método Get do endpoint /BTC/ticker retorna HttpStatus 200 OK
+	 * e ResponseBody válido.
+	 * 
+	 * curl --request GET \ url https://www.mercadobitcoin.net/api/BTC/ticker/
+	 * @author Emiliano Fagundes 
+	 * @Since 2019
+	 */
+	@Test
+	void whenGetInBtcTickerThenExpect200OkAndValidResponseBody() {
+		
+		String btcPath = "BTC/ticker/";
+		given()
+			.relaxedHTTPSValidation()
+			.when()
+			.get(BASE_URL + btcPath)
+			.then()
+			.statusCode(200)
+			.contentType(ContentType.JSON)
+			.body("ticker.high", is(anything()))
+			.body("ticker.low", is(anything()))
+			.body("ticker.vol", is(anything()))
+			.body("ticker.last", is(anything()))
+			.body("ticker.buy", is(anything()))
+			.body("ticker.sell", is(anything()))
+			.body("ticker.date", is(anything()));
+	}
+	
+	/**
+	 * Teste que verifica se o método Get do endpoint /BTC/ticker retorna HttpStatus 
+	 * 405Method Not Allowed
+	 */
+	@Test
+	void whenPostInBtcTickerPostThenExpect405MethodNotAllowed() {
+		
+		String btcPath = "BTC/ticker/";
+		given()
+			.relaxedHTTPSValidation()
+			.when()
+			.post(BASE_URL + btcPath)
+			.then()
+			.statusCode(405);
+	}
+	
+	
+	
+}
